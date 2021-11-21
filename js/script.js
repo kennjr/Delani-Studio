2979d8021a69f9c5ea1337be4f3ba9a5f7e36875
kennjr <ken.juniour@student.moringaschool.com>
2021-11-21 17:31:52 +0300

    Added the fun for the hover effect and linked it to all the project elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
     $(".design").click(() => {
         toggleElementVisibility("design_img");
         toggleElementVisibility("design_details");
@@ -13,9 +14,28 @@ $(document).ready(() => {
         toggleElementVisibility("product_management_img");
         toggleElementVisibility("product_management_details");
     });
+
+    addHoverEffect("1");
+    addHoverEffect("2");
+    addHoverEffect("3");
+    addHoverEffect("4");
+    addHoverEffect("5");
+    addHoverEffect("6");
+    addHoverEffect("7");
+    addHoverEffect("8");
 })
 
 // This fun will toggle the visibility of the element, should be called when an element has been clicked
 var toggleElementVisibility = (elementId) => {
     $("#" + elementId).slideToggle();
+}
+
+// This fun will adde the hover effect to an image and display the text ontop of it
+var addHoverEffect = (imageNum) => {
+    var overlayText = document.getElementById("overlay" + imageNum)
+    $("#portfolio_project_img_" + imageNum).hover(() => {
+        overlayText.style.visibility = "visible";
+    }, function(){
+        overlayText.style.visibility = "hidden";
+    })
 }
